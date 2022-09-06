5e1d1725e3d91aa91d93b23ea0043d3676d03975
Robin <stuckinga@gmail.com>
2022-09-06 09:37:21 -0400

    Update trip data

--- data/trips.ts
+++ data/trips.ts
@@ -1,3 +1,15 @@
+// Entries use this format
+// {
+//   vname: 'Ship name',
+//   port_origin: 'Port in Ukraine',
+//   departure: '2022-08-09',
+//   cargo: 'Corn, Wheat, etc',
+//   cargo_size: 10000,
+//   destination: 'Destination',
+//   status: 'Delivered, Shipped, etc'
+//   notes: 'Additional information'
+// }
+
 export const trips = [
   {
     vname: 'Razoni',
@@ -7,15 +19,16 @@ export const trips = [
     cargo_size: 26527,
     destination: 'Tartus, Syria?',
     status: 'Delivered',
-    notes: 'Original buyer refused delivery, suspected new delivery location'
+    notes:
+			'Original buyer refused to accept delivery, suspected new delivery location'
   },
   {
-    vname: 'Polamet',
+    vname: 'Polarnet',
     port_origin: 'Chornomorsk',
     departure: '2022-08-05',
     cargo: 'Corn',
     cargo_size: 12000,
-    destination: 'Karasu, Turkey',
+    destination: 'Karasu,Turkey',
     status: 'Delivered'
   },
   {
@@ -38,7 +51,7 @@ export const trips = [
   },
   {
     vname: 'Glory',
-    port_origin: 'Chomomorsk',
+    port_origin: 'Chornomorsk',
     departure: '2022-08-07',
     cargo: 'Corn',
     cargo_size: 66084,
@@ -56,7 +69,7 @@ export const trips = [
   },
   {
     vname: 'Mustafa Necati',
-    port_origin: 'Chomomorsk',
+    port_origin: 'Chornomorsk',
     departure: '2022-08-07',
     cargo: 'Sunflower oil',
     cargo_size: 6000,
@@ -65,7 +78,7 @@ export const trips = [
   },
   {
     vname: 'Star Helena',
-    port_origin: 'Chomomorsk',
+    port_origin: 'Chornomorsk',
     departure: '2022-08-07',
     cargo: 'Sunflower meal',
     cargo_size: 45000,
@@ -74,7 +87,7 @@ export const trips = [
   },
   {
     vname: 'Arizona',
-    port_origin: 'Chomomorsk',
+    port_origin: 'Chornomorsk',
     departure: '2022-08-08',
     cargo: 'Corn',
     cargo_size: 48459,
@@ -92,11 +105,733 @@ export const trips = [
   },
   {
     vname: 'Rahmi Yagci',
-    port_origin: 'Chomomorsk',
+    port_origin: 'Chornomorsk',
     departure: '2022-08-09',
     cargo: 'Sunflower meal',
     cargo_size: 5300,
     destination: 'Istanbul, Turkey',
     status: 'Delivered'
+  },
+  {
+    vname: 'Ocean Lion',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-09',
+    cargo: 'Corn',
+    cargo_size: 64720,
+    destination: 'Incheon, South Korea',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Sormovskiy 121',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-12',
+    cargo: 'Wheat',
+    cargo_size: 3050,
+    destination: 'Tekirdag, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Star Laura',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-12',
+    cargo: 'Corn',
+    cargo_size: 60150,
+    destination: 'Iran',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Fulmar S',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-13',
+    cargo: 'Corn',
+    cargo_size: 12000,
+    destination: 'Izmir, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Thoe',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-13',
+    cargo: 'Sunflower seed',
+    cargo_size: 2914,
+    destination: 'Tekirdag, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Osprey S',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-16',
+    cargo: 'Corn',
+    cargo_size: 11500,
+    destination: 'Iskenderun, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Brave Commander',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-16',
+    cargo: 'Wheat',
+    cargo_size: 23300,
+    destination: 'Djibouti',
+    status: 'Delivered',
+    notes: 'For Ethiopia, WFP'
+  },
+  {
+    vname: 'Bonita',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-16',
+    cargo: 'Corn',
+    cargo_size: 60000,
+    destination: 'Incheon, South Korea',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Ramus',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-16',
+    cargo: 'Wheat',
+    cargo_size: 6161,
+    destination: 'Karasu,Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Propus',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-16',
+    cargo: 'Wheat',
+    cargo_size: 9111,
+    destination: 'Constanta, Romania',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Petrel S',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-17',
+    cargo: 'Sunflower meal',
+    cargo_size: 18500,
+    destination: 'Amsterdam, The Netherlands',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Sara',
+    port_origin: 'Odesa',
+    departure: '2022-08-17',
+    cargo: 'Corn',
+    cargo_size: 8000,
+    destination: 'Bandirma, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Efe',
+    port_origin: 'Odesa',
+    departure: '2022-08-17',
+    cargo: 'Sunflower oil',
+    cargo_size: 7250,
+    destination: 'Toros, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'I Maria',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-18',
+    cargo: 'Corn',
+    cargo_size: 27982,
+    destination: 'Romania?',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Ocean S',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-20',
+    cargo: 'Wheat',
+    cargo_size: 25000,
+    destination: 'Marmara, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Zumrut Ana',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-20',
+    cargo: 'Sunflower oil',
+    cargo_size: 6300,
+    destination: 'Venice, Italy',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Foyle',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-21',
+    cargo: 'Sunflower oil',
+    cargo_size: 4300,
+    destination: 'Mersin, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Filyoz',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-21',
+    cargo: 'Sunflower oil',
+    cargo_size: 5000,
+    destination: 'Aliaga, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Kubrosli Y',
+    port_origin: 'Odesa',
+    departure: '2022-08-21',
+    cargo: 'Wheat',
+    cargo_size: 11000,
+    destination: 'Mersin, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Da Liang',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-21',
+    cargo: 'Sugar beet',
+    cargo_size: 14000,
+    destination: 'Kunsan, South Korea',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Great Arsenal',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-22',
+    cargo: 'Wheat',
+    cargo_size: 25500,
+    destination: 'Alexandria, Egypt',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Maranta',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-22',
+    cargo: 'Corn',
+    cargo_size: 5300,
+    destination: 'Greece',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Kafkametler',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-23',
+    cargo: 'Corn',
+    cargo_size: 2437,
+    destination: 'Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Ganosya',
+    port_origin: 'Odesa',
+    departure: '2022-08-24',
+    cargo: 'Corn',
+    cargo_size: 16500,
+    destination: 'Libya',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Zhe Hai 505',
+    port_origin: 'Odesa',
+    departure: '2022-08-24',
+    cargo: 'Rapeseed',
+    cargo_size: 29600,
+    destination: 'The Netherlands',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Mohamad Y',
+    port_origin: 'Odesa',
+    departure: '2022-08-25',
+    cargo: 'Wheat',
+    cargo_size: 11000,
+    destination: 'Israel',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Ascanios',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-25',
+    cargo: 'Corn',
+    cargo_size: 58510,
+    destination: 'Brake, Germany',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Bellis',
+    port_origin: 'Odesa',
+    departure: '2022-08-25',
+    cargo: 'Soybean',
+    cargo_size: 6000,
+    destination: 'Haifa, Israel',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Zelek Star',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-26',
+    cargo: 'Pea',
+    cargo_size: 3700,
+    destination: 'Mersin, Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Oris Sofi',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-26',
+    cargo: 'Sunflower oil',
+    cargo_size: 5900,
+    destination: 'Tekirdag, Turkey',
+    status: 'Delivered'
+  },
+  {
+    vname: 'Aviva',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-26',
+    cargo: 'Sunflower oil',
+    cargo_size: 19100,
+    destination: 'India',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Sea Eagle',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-26',
+    cargo: 'Wheat',
+    cargo_size: 63340,
+    destination: 'Sudan',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Pretty Lady',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-26',
+    cargo: 'Corn',
+    cargo_size: 45000,
+    destination: 'Egypt',
+    status: 'Shipped'
+  },
+  {
+    vname: 'S-Breeze',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-27',
+    cargo: 'Corn',
+    cargo_size: 44000,
+    destination: 'Egypt',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Adnan Torlak',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-27',
+    cargo: 'Rapeseed',
+    cargo_size: 14750,
+    destination: 'France',
+    status: 'Shipped'
+  },
+  {
+    vname: 'SSI Invincible II',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-28',
+    cargo: 'Barley',
+    cargo_size: 49500,
+    destination: 'China',
+    status: 'Shipped'
+  },
+  {
+    vname: 'DS Sofie Bulker',
+    port_origin: 'Odesa',
+    departure: '2022-08-28',
+    cargo: 'Barley',
+    cargo_size: 27000,
+    destination: 'Spain',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Aeolos',
+    port_origin: 'Odesa',
+    departure: '2022-08-28',
+    cargo: 'Corn',
+    cargo_size: 30000,
+    destination: 'Rotterdam, The Netherlands',
+    status: 'Shipped'
+  },
+  {
+    vname: 'FPMC B 201',
+    port_origin: 'Odesa',
+    departure: '2022-08-28',
+    cargo: 'Wheat',
+    cargo_size: 33000,
+    destination: 'Egypt',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Anastasia',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-28',
+    cargo: 'Wheat',
+    cargo_size: 3800,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Silver Lady',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-28',
+    cargo: 'Corn',
+    cargo_size: 39500,
+    destination: 'Italy',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Melina',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-28',
+    cargo: 'Corn',
+    cargo_size: 27500,
+    destination: 'Romania',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Peace M',
+    port_origin: 'Odesa',
+    departure: '2022-08-29',
+    cargo: 'Corn',
+    cargo_size: 24800,
+    destination: 'Romania',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Ash Baltic',
+    port_origin: 'Odesa',
+    departure: '2022-08-29',
+    cargo: 'Corn',
+    cargo_size: 11000,
+    destination: 'Egypt',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Karteria',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-30',
+    cargo: 'Wheat',
+    cargo_size: 37500,
+    destination: 'Turkey',
+    status: 'Shipped',
+    notes: 'For Yemen, WFP'
+  },
+  {
+    vname: 'Saffet Aga',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-30',
+    cargo: 'Corn',
+    cargo_size: 2236,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Seajoy',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-30',
+    cargo: 'Corn',
+    cargo_size: 64215,
+    destination: 'Spain',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Lady Zehma',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-30',
+    cargo: 'Corn',
+    cargo_size: 3000,
+    destination: 'Italy',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Michalis',
+    port_origin: 'Odesa',
+    departure: '2022-08-30',
+    cargo: 'Corn',
+    cargo_size: 33000,
+    destination: 'Spain',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Simas',
+    port_origin: 'Odesa',
+    departure: '2022-08-30',
+    cargo: 'Wheat',
+    cargo_size: 15000,
+    destination: 'Italy',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Afanasiy Matyushenko',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-31',
+    cargo: 'Wheat',
+    cargo_size: 3000,
+    destination: 'Tekirdag, Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Maran Astronomer',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-31',
+    cargo: 'Corn',
+    cargo_size: 74500,
+    destination: 'Spain',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Lady Aillar',
+    port_origin: 'Yuzhny',
+    departure: '2022-08-31',
+    cargo: 'Corn',
+    cargo_size: 27000,
+    destination: 'Spain',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Hazar S',
+    port_origin: 'Chornomorsk',
+    departure: '2022-08-31',
+    cargo: 'Corn',
+    cargo_size: 5500,
+    destination: 'Bandirma, Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Daytona Dynamic',
+    port_origin: 'Odesa',
+    departure: '2022-09-01',
+    cargo: 'Corn',
+    cargo_size: 25850,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Eneida (wheat)',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-01',
+    cargo: 'Wheat',
+    cargo_size: 35470,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Eneida (corn)',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-01',
+    cargo: 'Corn',
+    cargo_size: 8560,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Helga',
+    port_origin: 'Odesa',
+    departure: '2022-09-01',
+    cargo: 'Corn',
+    cargo_size: 27000,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Massa J',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-01',
+    cargo: 'Wheat',
+    cargo_size: 28500,
+    destination: 'Turkey?',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Fulmar S',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-02',
+    cargo: 'Sunflower seed',
+    cargo_size: 9835,
+    destination: 'Bulgaria',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Stella GS',
+    port_origin: 'Odesa',
+    departure: '2022-09-02',
+    cargo: 'Corn',
+    cargo_size: 30300,
+    destination: 'Israel',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Mubariz Ibrahimov',
+    port_origin: 'Odesa',
+    departure: '2022-09-02',
+    cargo: 'Sunflower oil',
+    cargo_size: 6600,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Spring',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-02',
+    cargo: 'Wheat',
+    cargo_size: 8500,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Canga Star',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-02',
+    cargo: 'Rapeseed',
+    cargo_size: 7000,
+    destination: 'France',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Katsuyama',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-02',
+    cargo: 'Sunflower oil',
+    cargo_size: 21000,
+    destination: 'India',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Mavka',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-02',
+    cargo: 'Sunflower oil',
+    cargo_size: 12500,
+    destination: 'Romania',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Nord Virgo',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-02',
+    cargo: 'Corn',
+    cargo_size: 62340,
+    destination: 'China',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Maina',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-03',
+    cargo: 'Corn',
+    cargo_size: 56500,
+    destination: 'Spain',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Canopus',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-03',
+    cargo: 'Sunflower oil',
+    cargo_size: 42000,
+    destination: 'India',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Sara',
+    port_origin: 'Odesa',
+    departure: '2022-09-03',
+    cargo: 'Wheat',
+    cargo_size: 6600,
+    destination: 'Tekirdag, Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Lady Eva',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-03',
+    cargo: 'Wheat',
+    cargo_size: 6117,
+    destination: 'Greece',
+    status: 'Shipped'
+  },
+  {
+    vname: 'BC Callisto',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-03',
+    cargo: 'Wheat',
+    cargo_size: 31400,
+    destination: 'Egypt',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Sealock',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-03',
+    cargo: 'Pea',
+    cargo_size: 2070,
+    destination: 'Mersin, Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Sea Dolphin C',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-03',
+    cargo: 'Rapeseed',
+    cargo_size: 31098,
+    destination: 'Amsterdam, The Netherlands',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Lady Perla',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-03',
+    cargo: 'Corn',
+    cargo_size: 20500,
+    destination: 'Italy',
+    status: 'Shipped'
+  },
+  {
+    vname: 'MRC Lina',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-04',
+    cargo: 'Sunflower oil',
+    cargo_size: 7800,
+    destination: 'Mersin, Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Gen. Polad Hashimov',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-04',
+    cargo: 'Sunflower oil',
+    cargo_size: 6450,
+    destination: 'Mersin, Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'MV Golden Yara',
+    port_origin: 'Yuzhny',
+    departure: '2022-09-05',
+    cargo: 'Wheat',
+    cargo_size: 13500,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'Baron',
+    port_origin: 'Odesa',
+    departure: '2022-09-05',
+    cargo: 'Corn',
+    cargo_size: 5700,
+    destination: 'Turkey',
+    status: 'Shipped'
+  },
+  {
+    vname: 'My Meray',
+    port_origin: 'Chornomorsk',
+    departure: '2022-09-05',
+    cargo: 'Corn',
+    cargo_size: 30000,
+    destination: 'Egypt',
+    status: 'Shipped'
   }
 ];
