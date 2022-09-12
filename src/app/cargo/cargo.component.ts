e0a718cd6ee06aff98bcd1600fb202fab560e165
Robin <stuckinga@gmail.com>
2022-09-12 01:28:14 -0400

    Add totals to cargo table

--- src/app/cargo/cargo.component.ts
+++ src/app/cargo/cargo.component.ts
@@ -10,8 +10,10 @@ import {
 
 interface Cargo {
 	name: string;
-	size: number;
 	trips: number;
+	on_ship: number;
+	delivered: number;
+	total: number;
 }
 
 @Component({
@@ -25,7 +27,13 @@ export class CargoComponent implements OnInit {
 	drawCheckboxInRowHeaders = drawCheckboxInRowHeaders;
 	addClassesToRows = addClassesToRows;
 	changeCheckboxCell = changeCheckboxCell;
-	colHeaders = ['Commodity', 'Total Shipped Size (mt)', 'Number of Shipments'];
+	colHeaders = [
+		'Commodity',
+		'Number of Shipments',
+		'On ships (mt)',
+		'Delivered (mt)',
+		'Total (On ships + Delivered)',
+	];
 
 	ngOnInit(): void {
 		this.getCargo();
@@ -33,27 +41,65 @@ export class CargoComponent implements OnInit {
 
 	getCargo(): void {
 		const sums: Cargo[] = [];
+		const allTotal = <Cargo>{
+			name: 'Total',
+			trips: 0,
+			on_ship: 0,
+			delivered: 0,
+			total: 0,
+		};
 		getTripData().forEach((i) => {
 			if (Array.isArray(i.cargo)) {
 				// If multiple types of cargo in one ship
+				// Todo: DRY cleanup
 				i.cargo.forEach((j) => {
 					if (sums.find((c) => c.name === j.cargo) == null) {
-						sums.push({ name: j.cargo, size: 0, trips: 0 });
+						sums.push({
+							name: j.cargo,
+							trips: 0,
+							on_ship: 0,
+							delivered: 0,
+							total: 0,
+						});
 					}
 					const c = sums.find((c) => c.name === j.cargo);
-					c.size += j.cargo_size;
+					i.status === 'Shipped'
+						? (c.on_ship += j.cargo_size)
+						: (c.delivered += j.cargo_size);
+					c.total += j.cargo_size;
 					c.trips += 1;
+
+					i.status === 'Shipped'
+						? (allTotal.on_ship += j.cargo_size)
+						: (allTotal.delivered += j.cargo_size);
+					allTotal.total += j.cargo_size;
+					allTotal.trips += 1;
 				});
 			} else {
 				// If single type of cargo in one ship
 				if (sums.find((c) => c.name === i.cargo) == null) {
-					sums.push({ name: i.cargo, size: 0, trips: 0 });
+					sums.push({
+						name: i.cargo,
+						trips: 0,
+						on_ship: 0,
+						delivered: 0,
+						total: 0,
+					});
 				}
 				const c = sums.find((c) => c.name === i.cargo);
-				c.size += i.cargo_size;
+				i.status === 'Shipped'
+					? (c.on_ship += i.cargo_size)
+					: (c.delivered += i.cargo_size);
+				c.total += i.cargo_size;
 				c.trips += 1;
+
+				i.status === 'Shipped'
+					? (allTotal.on_ship += i.cargo_size)
+					: (allTotal.delivered += i.cargo_size);
+				allTotal.total += i.cargo_size;
+				allTotal.trips += 1;
 			}
 		});
-		this.dataset.push(...sums);
+		this.dataset.push(...sums, allTotal);
 	}
 }
